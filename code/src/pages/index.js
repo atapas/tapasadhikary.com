45f02a4006cfcd0c9ac7b440fd2b9d0caecc9b34
Tapas Adhikary <tapas.adhikary@gmail.com>
2019-10-10 09:40:24 +0530

    initial structure

---
+++ code/src/pages/index.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <>
+    <SEO title="Home" />
+    <h1>Hi there</h1>
+    <p>Welcome to tapasadhikary.com.</p>
+    <p>Now go build something great.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </>
+ 
+)
+
+export default IndexPage
