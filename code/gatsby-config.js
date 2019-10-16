19bb2cdf94981a095df1c4b1e90b4dd4b7b9af0e
Tapas Adhikary <tapas.adhikary@gmail.com>
2019-10-16 09:27:07 +0530

    Corrected the title

--- code/gatsby-config.js
+++ code/gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `tapasadhikary.com: Knowledge Gained === Knowledge Shared!`,
+    description: `Personal website for @tapasadhikary`,
     author: `@gatsbyjs`,
   },
   plugins: [
